d76230ee8e28695295eff281526731f39a2f2a48
Olly Oechsle <owksley@gmail.com>
2012-11-17 18:04:23 +0000

    Has class and has text tests

--- test/jquery-tests.js
+++ test/jquery-tests.js
@@ -8,11 +8,29 @@
         }
     });
 
-    test("gets top keywords when no tags", function () {
+    test("hidden and visible", function () {
 
         thenThe(jQuery("<div></div>")).should(beVisible);
 
         thenThe(jQuery("<div class='hidden'></div>")).should(beHidden);
+
+    });
+
+    test("has class", function () {
+
+        thenThe(jQuery("<div class='hidden'></div>")).should(haveClass("hidden"));
+
+        thenThe(jQuery("<div class='foo'></div>")).should(haveClass("foo"));
+
+        thenThe(jQuery("<div class='foo'></div>")).should(notHaveClass("hidden"));
+
+        thenThe(jQuery("<div></div>")).should(notHaveClass("foo"));
+
+    });
+
+    test("has text", function () {
+
+        thenThe(jQuery("<div'>Hello, World</div>")).should(haveText("Hello, World"));
 
     });
 
